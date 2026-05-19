0bdd60492ac6a599ccb7a734704b42b36e89fe6c
Felipe Komata <fbkomata@gmail.com>
2026-05-18 21:10:53 -0300

    adição do .hpp no minefield

---
+++ minefield.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include "tiles.hpp"
+
+class Minefield
+{
+private:
+    std::vector<std::vector<Tiles>> matriz;
+    int coord_x;
+    int coord_y;
+    int q_bombas;
+    bool primeira_tentativa;
+    int tiles_revelados;
+
+    void cascata(int i, int j);
+public:
+    Minefield(int coord_x,int coord_y,int q_bombas);
+    void gera_matriz();
+    void calcula_bombas_proximas();
+    void escavar(int i , int j);
+    void botar_bandeira(int i,int j);
+    bool verifica_vitoria();
+    void imprimir();
+
+};
