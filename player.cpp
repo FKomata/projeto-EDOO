c3b966ca93662493575ee895b9404e1464bdaa4e
Gabriel Ribeiro <gabrielcarleial@gmail.com>
2026-05-24 19:57:11 -0300

    feat: update player logic and coletables logic in minefield

--- player.cpp
+++ player.cpp
@@ -1,8 +1,12 @@
 #include "player.hpp"
 
 Player::Player(int vidas, int bandeiras)
-    : vidas(vidas), vidas_max(vidas), pontos(0), bandeiras(bandeiras), bandeiras_max(bandeiras)
+    : vidas(vidas), vidas_max(vidas), pontos(0), 
+      bandeiras(bandeiras), bandeiras_max(bandeiras), 
+      tempo_restante(0), tempo_limite(0), timer_ativado(false)
 {}
+
+// Metodos de status geral
 
 void Player::tomar_dano()
 {
@@ -10,10 +14,24 @@ void Player::tomar_dano()
         vidas--;
 }
 
+void Player::curar_vida(int quantidade)
+{
+    vidas += quantidade;
+    if (vidas > vidas_max) 
+        vidas = vidas_max; // Limita a cura ao máximo
+}
+
 void Player::pontuar(int quantidade)
 {
     pontos += quantidade;
 }
+
+bool Player::esta_vivo() const
+{
+    return vidas > 0;
+}
+
+// Metodos de bandeira
 
 bool Player::usar_bandeira()
 {
@@ -31,10 +49,58 @@ void Player::recuperar_bandeira()
         bandeiras++;
 }
 
-bool Player::esta_vivo() const
+void Player::coletar_bandeira(int quantidade)
 {
-    return vidas > 0;
+    bandeiras += quantidade;
+    if (bandeiras > bandeiras_max)
+        bandeiras_max = bandeiras; // Coletável aumenta o limite máximo
 }
+
+// Metodos de tempo
+
+void Player::iniciar_timer(int limite_segundos)
+{
+    tempo_limite = limite_segundos;
+    tempo_restante = limite_segundos;
+    timer_ativado = true;
+}
+
+void Player::decrementar_tempo(int segundos)
+{
+    if (timer_ativado)
+    {
+        tempo_restante -= segundos;
+        if (tempo_restante <= 0)
+        {
+            tempo_restante = 0;
+            timer_ativado = false;
+        }
+    }
+}
+
+void Player::adicionar_tempo(int segundos)
+{
+    if (timer_ativado)
+    {
+        tempo_restante += segundos;
+        if (tempo_restante > tempo_limite)
+        {
+            tempo_restante = tempo_limite; // Não deixa passar do limite original
+        }
+    }
+}
+
+bool Player::tempo_esgotado() const
+{
+    return tempo_restante <= 0;
+}
+
+void Player::desativar_timer()
+{
+    timer_ativado = false;
+}
+
+// Getters
 
 int Player::get_vidas() const
 {
@@ -60,3 +126,7 @@ int Player::get_bandeiras_max() const
 {
     return bandeiras_max;
 }
+
+int Player::get_tempo_restante() const { return tempo_restante; }
+
+bool Player::is_timer_ativado() const { return timer_ativado; }
