9527ec4c91d8662a09ec93c76a7009cf05b66108
PedroWellington <eupedrowell@gmail.com>
2026-05-25 21:19:13 -0300

    minefild e main refatorados

--- main.cpp
+++ main.cpp
@@ -1,29 +1,55 @@
 #include "game.hpp"
 
-int main() {
+int main()
+{
     GameManager game;
 
-    // Criando o script da nossa simulação textual!
-    // As coordenadas foram pensadas baseadas nas posições fixas do seu Minefield().
     std::vector<Move> roteiro = {
-        // 1. Testa escavação segura (Cascata)
-        {7, 7, ActionType::DIG},  
-        
-        // 2. Testa colocar uma bandeira (Bandeira em 0,5 onde sabemos que tem bomba)
-        {0, 5, ActionType::FLAG}, 
-        
-        // 3. Testa pegar coletável de Vida (Posição 2,3)
-        {2, 3, ActionType::DIG},  
-        
-        // 4. Testa tomar dano numa bomba (Bomba no 1,2)
-        {1, 2, ActionType::DIG},  
+        // 1. Primeira jogada exigida no 0,0 (Cascata pequena, pois há bombas próximas)
+        {0, 0, ActionType::DIG},
 
-        // 5. Pegar coletável de Tempo (Posição 1,4)
-        {1, 4, ActionType::DIG}
-        
-        // Você e seu time podem adicionar mais jogadas aqui pra provar pro professor 
-        // que o jogo roda até acabar as vidas ou até limpar o campo.
-    };
+        // 2 e 3. Usar bandeiras (Marcando as bombas do topo)
+        {0, 4, ActionType::FLAG},
+        {1, 4, ActionType::FLAG},
+
+        // 4 e 5. Perder duas vidas (Pisando nas bombas isoladas da esquerda)
+        {2, 2, ActionType::DIG}, // BOOM! (-1 vida)
+        {3, 0, ActionType::DIG}, // BOOM! (-1 vida)
+
+        // 6 a 11. Coletar Vidas e Itens
+        {2, 3, ActionType::DIG}, // +1 Vida
+        {5, 1, ActionType::DIG}, // +1 Vida
+        {1, 5, ActionType::DIG}, // +10 Segundos
+        {6, 2, ActionType::DIG}, // +10 Segundos
+        {3, 1, ActionType::DIG}, // +1 Bandeira
+        {4, 6, ActionType::DIG}, // +1 Bandeira
+
+        // 12 a 14. Cliques em áreas vazias (0) para abrir as cascatas das bordas
+        {2, 7, ActionType::DIG}, // Abre a direita inteira
+        {7, 2, ActionType::DIG}, // Abre o fundo esquerdo
+        {7, 7, ActionType::DIG}, // Abre o fundo direito
+
+        // 15 em diante. Limpeza manual dos números restantes entre as bombas para VENCER
+        {0, 3, ActionType::DIG},
+        {0, 5, ActionType::DIG},
+        {1, 2, ActionType::DIG},
+        {1, 3, ActionType::DIG},
+        {2, 1, ActionType::DIG},
+        {2, 4, ActionType::DIG},
+        {3, 2, ActionType::DIG},
+        {3, 3, ActionType::DIG},
+        {4, 1, ActionType::DIG},
+        {4, 2, ActionType::DIG},
+        {5, 0, ActionType::DIG},
+        {5, 2, ActionType::DIG},
+        {5, 4, ActionType::DIG},
+        {5, 5, ActionType::DIG},
+        {5, 7, ActionType::DIG},
+        {6, 1, ActionType::DIG},
+        {6, 3, ActionType::DIG},
+        {7, 0, ActionType::DIG},
+        {7, 4, ActionType::DIG},
+        {7, 5, ActionType::DIG}};
 
     game.rodar_simulacao(roteiro);
 
--- minefield.cpp
+++ minefield.cpp
@@ -1,48 +1,41 @@
 #include "minefield.hpp"
 
-
 Minefield::Minefield(int i, int j)
-   : coord_x(i), coord_y(j), primeira_tentativa(true), tiles_revelados(0)
+    : coord_x(i), coord_y(j), primeira_tentativa(true), tiles_revelados(0)
 {
     for (int li = 0; li < coord_x; li++)
-   {
-       std::vector<Tiles> row;
-       for (int lj = 0; lj < coord_y; lj++)
-           row.emplace_back(false);
-       matriz.push_back(row);
-   }
+    {
+        std::vector<Tiles> row;
+        for (int lj = 0; lj < coord_y; lj++)
+            row.emplace_back(false);
+        matriz.push_back(row);
+    }
 
+    std::vector<std::pair<int, int>> posicoes_bombas = {
+        {0, 4}, {1, 4}, {2, 2}, {3, 0}, {4, 0}, {5, 3}, {5, 6}, {6, 0}, {6, 4}, {6, 5}};
+    std::vector<std::pair<int, int>> posicoes_vida = {{2, 3}, {5, 1}};
+    std::vector<std::pair<int, int>> posicoes_tempo = {{1, 5}, {6, 2}};
+    std::vector<std::pair<int, int>> posicoes_bandeira = {{3, 1}, {4, 6}};
 
-    std::vector<std::pair<int,int>> posicoes_bombas = {
-       {0, 5}, {1, 2}, {2, 7}, {3, 4},
-       {4, 0}, {5, 3}, {6, 6}, {7, 1}
-   };
-    std::vector<std::pair<int,int>> posicoes_vida = { {2,3}, {5,1} };
-    std::vector<std::pair<int,int>> posicoes_tempo = { {1,4}, {6,2} };
-    std::vector<std::pair<int,int>> posicoes_bandeira = { {3,0}, {4,6} };
+    for (auto &[bi, bj] : posicoes_bombas)
+        matriz[bi][bj] = Tiles(true);
 
-    for (auto& [bi, bj] : posicoes_bombas)
-       matriz[bi][bj] = Tiles(true);
-    
-    for (auto& [bi, bj] : posicoes_vida)
-       matriz[bi][bj].set_coletavel(Coletavel::vida);
+    for (auto &[bi, bj] : posicoes_vida)
+        matriz[bi][bj].set_coletavel(Coletavel::vida);
 
-    for (auto& [bi, bj] : posicoes_tempo)
-       matriz[bi][bj].set_coletavel(Coletavel::tempo);
+    for (auto &[bi, bj] : posicoes_tempo)
+        matriz[bi][bj].set_coletavel(Coletavel::tempo);
 
-    for (auto& [bi, bj] : posicoes_bandeira)
-       matriz[bi][bj].set_coletavel(Coletavel::bandeira);
+    for (auto &[bi, bj] : posicoes_bandeira)
+        matriz[bi][bj].set_coletavel(Coletavel::bandeira);
 
-
-
-   calcula_bombas_proximas();
+    calcula_bombas_proximas();
 }
 
 void Minefield::calcula_bombas_proximas()
 {
-    const int di[] = {-1,-1,-1, 0, 0, 1, 1, 1};
-    const int dj[] = {-1, 0, 1,-1, 1,-1, 0, 1};
-
+    const int di[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+    const int dj[] = {-1, 0, 1, -1, 1, -1, 0, 1};
 
     for (int i = 0; i < coord_x; i++)
     {
@@ -66,21 +59,19 @@ void Minefield::calcula_bombas_proximas()
 
 void Minefield::mover_bomba(int bomb_i, int bomb_j)
 {
-   for (int i = 0; i < coord_x; i++)
-   {
-       for (int j = 0; j < coord_y; j++)
-       {
-           if (!matriz[i][j].get_tembomba())
-           {
-               matriz[bomb_i][bomb_j] = Tiles(false);
-               matriz[i][j] = Tiles(true);
-               return;
-           }
-       }
-   }
+    for (int i = 0; i < coord_x; i++)
+    {
+        for (int j = 0; j < coord_y; j++)
+        {
+            if (!matriz[i][j].get_tembomba())
+            {
+                matriz[bomb_i][bomb_j] = Tiles(false);
+                matriz[i][j] = Tiles(true);
+                return;
+            }
+        }
+    }
 }
-
-
 
 ResultadoEscavacao Minefield::escavar(int i, int j)
 {
@@ -104,14 +95,12 @@ ResultadoEscavacao Minefield::escavar(int i, int j)
         }
     }
 
-
     if (matriz[i][j].get_tembomba())
     {
         matriz[i][j].set_tipo_tile(Tipo_tile::bomba_explodida);
         tiles_revelados++;
         return ResultadoEscavacao::bomba;
     }
-
 
     if (matriz[i][j].get_bombasproximas() == 0)
         cascata(i, j);
@@ -122,38 +111,36 @@ ResultadoEscavacao Minefield::escavar(int i, int j)
     }
 
     // Tratamento dos coletaveis
-    if (coletavel_tile != Coletavel::nenhum) 
+    if (coletavel_tile != Coletavel::nenhum)
     {
         // Remove o coletável do chão para não pegar de novo
-        matriz[i][j].set_coletavel(Coletavel::nenhum); 
+        matriz[i][j].set_coletavel(Coletavel::nenhum);
 
         // Retorna o aviso
-        if (coletavel_tile == Coletavel::vida) return ResultadoEscavacao::coletou_vida;
-        if (coletavel_tile == Coletavel::tempo) return ResultadoEscavacao::coletou_tempo;
-        if (coletavel_tile == Coletavel::bandeira) return ResultadoEscavacao::coletou_bandeira;
+        if (coletavel_tile == Coletavel::vida)
+            return ResultadoEscavacao::coletou_vida;
+        if (coletavel_tile == Coletavel::tempo)
+            return ResultadoEscavacao::coletou_tempo;
+        if (coletavel_tile == Coletavel::bandeira)
+            return ResultadoEscavacao::coletou_bandeira;
     }
-
 
     return ResultadoEscavacao::livre;
 }
-
 
 void Minefield::cascata(int i, int j)
 {
     matriz[i][j].set_tipo_tile(Tipo_tile::revelado);
     tiles_revelados++;
 
-
     if (matriz[i][j].get_bombasproximas() > 0)
         return;
 
-
-    const int di[] = {-1,-1,-1, 0, 0, 1, 1, 1};
-    const int dj[] = {-1, 0, 1,-1, 1,-1, 0, 1};
-
+    const int di[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+    const int dj[] = {-1, 0, 1, -1, 1, -1, 0, 1};
 
     for (int k = 0; k < 8; k++)
-   {
+    {
         int ni = i + di[k];
         int nj = j + dj[k];
         if (ni >= 0 && ni < coord_x && nj >= 0 && nj < coord_y)
@@ -161,18 +148,15 @@ void Minefield::cascata(int i, int j)
             if (matriz[ni][nj].get_estado_atual() == Tipo_tile::coberto)
                 cascata(ni, nj);
         }
-   }
+    }
 }
 
-
-
-void Minefield::bandeira(int i , int j)
+void Minefield::bandeira(int i, int j)
 {
-    if(matriz[i][j].get_estado_atual() == Tipo_tile::bandeira || matriz[i][j].get_estado_atual() == Tipo_tile::coberto)
+    if (matriz[i][j].get_estado_atual() == Tipo_tile::bandeira || matriz[i][j].get_estado_atual() == Tipo_tile::coberto)
     {
         matriz[i][j].botar_bandeira();
     }
-
 }
 
 bool Minefield::verifica_vitoria()
@@ -187,18 +171,17 @@ bool Minefield::verifica_vitoria()
 void Minefield::imprimir()
 {
     std::cout << "CAMPO MINADO : " << std::endl;
-    for(int i = 0; i < coord_x;i++)
+    for (int i = 0; i < coord_x; i++)
     {
-        for(int j = 0 ; j < coord_y; j++)
+        for (int j = 0; j < coord_y; j++)
         {
             std::cout << matriz[i][j].get_conteudo_tile() << ' ';
         }
         std::cout << std::endl;
     }
-
 }
 
 Tipo_tile Minefield::get_tile_estado(int i, int j)
 {
-   return matriz[i][j].get_estado_atual();
+    return matriz[i][j].get_estado_atual();
 }
